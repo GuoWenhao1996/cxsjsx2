ec619a2aebdcc9b84f3e0568a61bd6c18a40602c
GuoWenhao1996 <1842297753@qq.com>
2017-06-07 01:07:01 +0800

    完善评价dao层 v1.3

--- code/java_web/UniversityOfShaft/src/dao/AssessDao.java
+++ code/java_web/UniversityOfShaft/src/dao/AssessDao.java
@@ -2,17 +2,19 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import entity.AssessEntity;
 import util.DBUtil;
 
 /**
- * 2017-6-6 14:14:46
+ * 2017-6-7 01:04:24
  * 
  * 评价dao层，完成数据库交互任务
  * 
  * @author guowenhao
- * @version 1.0
+ * @version 1.3
  */
 public class AssessDao {
 
@@ -42,4 +44,103 @@ public class AssessDao {
 		// 执行sql
 		ps.executeUpdate();
 	}
+
+	/**
+	 * 插入学生对学生的评价信息
+	 * 
+	 * @param ae
+	 *            评价实体
+	 * @throws Exception
+	 *             系统异常
+	 */
+	public void insertStuAss(AssessEntity ae) throws Exception {
+		// 获取数据库连接
+		Connection connection = DBUtil.getConnection();
+
+		// 构建sql语句
+		StringBuilder sql = new StringBuilder();
+		sql.append(" insert into t_stuassess ")
+				.append(" (StuAss_Id,Stu_SNo,StuAss_SNo,StuAss_DataTime,StuAss_Context) ").append(" values ")
+				.append(" (?,?,?,?,?) ");
+		// 为sql传入参数
+		PreparedStatement ps = connection.prepareStatement(sql.toString());
+		ps.setString(1, ae.getA_Id());
+		ps.setString(2, ae.getStu_SNo());
+		ps.setString(3, ae.getA_PersonNo());
+		ps.setString(4, ae.getA_DataTime());
+		ps.setString(5, ae.getA_Context());
+		// 执行sql
+		ps.executeUpdate();
+	}
+
+	/**
+	 * 修改自我评价
+	 * 
+	 * @param ae
+	 *            评价实体
+	 * @throws Exception
+	 *             系统异常
+	 */
+	public void updateStuSelfAss(AssessEntity ae) throws Exception {
+		// 获取数据库连接
+		Connection connection = DBUtil.getConnection();
+
+		// 构建sql语句
+		StringBuilder sql = new StringBuilder();
+		sql.append(" update t_stuinfo ").append(" set  Stu_Assess=? ").append(" where Stu_SNo=? ");
+		// 为sql传入参数
+		PreparedStatement ps = connection.prepareStatement(sql.toString());
+		ps.setString(1, ae.getA_Context());
+		ps.setString(2, ae.getStu_SNo());
+		// 执行sql
+		ps.executeUpdate();
+	}
+
+	/**
+	 * 通过评价ID找到对应的评价实体
+	 * 
+	 * @param teaAssID
+	 *            评价ID
+	 * @return 评价实体对象
+	 * @throws Exception
+	 *             运行错误
+	 */
+	public AssessEntity showAssess(String teaAssID) throws Exception {
+		// 获取数据库连接
+		Connection connection = DBUtil.getConnection();
+
+		// 构建sql语句
+		StringBuilder sql = new StringBuilder();
+		sql.append(" select * from t_tassess where TeaAss_ID=? ");
+		// 为sql传入参数
+		PreparedStatement ps = connection.prepareStatement(sql.toString());
+		ps.setString(1, teaAssID);
+		// 构建游标并执行
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		// 调用rowsEntity()将游标数据转化为实体
+		return (this.rowsEntity(rs));
+	}
+
+	/**
+	 * 将数据库查询出的数据转化为实体
+	 * 
+	 * @param rs
+	 *            游标
+	 * @return 转化成功的实体
+	 * @throws SQLException
+	 *             数据库错误
+	 */
+	private AssessEntity rowsEntity(ResultSet rs) throws SQLException {
+		// 初始化实体
+		AssessEntity ae = new AssessEntity();
+		// 1、2、3、4、5分别为评价id、被评价学生学号、评价人、评价时间、评价内容
+		ae.setA_Id(rs.getString(1));
+		ae.setStu_SNo(rs.getString(2));
+		ae.setA_PersonNo(rs.getString(3));
+		ae.setA_DataTime(rs.getString(4));
+		ae.setA_Context(rs.getString(5));
+		return ae;
+	}
+
 }
--- code/java_web/UniversityOfShaft/src/test/AssessTest.java
+++ code/java_web/UniversityOfShaft/src/test/AssessTest.java
@@ -2,29 +2,53 @@ package test;
 
 import dao.AssessDao;
 import entity.AssessEntity;
+import util.DBUtil;
+import util.ThisSystemUtil;
 
 /**
- * 2017-6-6 14:43:02
+ * 2017-6-7 01:04:29
  * 
  * 评价测试类，用于测试实体、dao
  * 
  * @author guowenhao
- * @version 1.1
+ * @version 1.3
  */
 public class AssessTest {
 	public static void main(String[] args) throws Exception {
-		// 创建实体并赋值
-		AssessEntity ae1 = new AssessEntity();
-		ae1.setA_Id("cscscs");
-		ae1.setStu_SNo("631406010109");
-		ae1.setA_PersonNo("1001");
-		ae1.setA_DataTime("2017-6-6 14:41:03");
-		ae1.setA_Context("单元测试之插入测试");
-		System.out.println(ae1.toString());
+		// // 创建实体并赋值
+		// AssessEntity ae1 = new AssessEntity();
+		// ae1.setA_Id(DBUtil.uuid());
+		// ae1.setStu_SNo("631406010109");
+		// ae1.setA_PersonNo("1001");
+		// ae1.setA_DataTime(ThisSystemUtil.getSystemTime());
+		// ae1.setA_Context("单元测试之插入测试uuid");
+		// System.out.println(ae1.toString());
+		//
+		// ae1.setA_PersonNo("631406010102");
+		// ae1.setA_Context("大大大帅哥！");
+		// System.out.println(ae1.toString());
 
-		// dao层初始化
+		// // 目前没有个人信息类，暂时先用评价类作为实体测试
+		// AssessEntity ae2 = new AssessEntity();
+		// ae2.setA_Context("自我评价修改测试！");
+		// ae2.setStu_SNo("631406010109");
+		// // System.out.println(ae1.toString());
+
+		// dao层初始化测试
 		AssessDao ad = new AssessDao();
-		ad.insertTeacAss(ae1);
+		// //教师评价插入测试
+		// ad.insertTeacAss(ae1);
 
+		// //学生评价插入测试
+		// ad.insertStuAss(ae1);
+
+		// //修改自我评价测试
+		// ad.updateStuSelfAss(ae2);
+
+		// 数据库内容转化为实体测试
+		AssessEntity ae1 = ad.showAssess("3fb1ad3206fa37d97d24d30bd41ca7be");
+		AssessEntity ae2 = ad.showAssess("45d44fa3113c640b9d3449de617b058f");
+		System.out.println(ae1.toString());
+		System.out.println(ae2.toString());
 	}
 }
